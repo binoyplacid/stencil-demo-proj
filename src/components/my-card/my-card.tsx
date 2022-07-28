81087509565dbe093ea4be22444aaa8b56c58e84
binoyplacid <35947298+binoyplacid@users.noreply.github.com>
2022-07-28 00:49:50 -0400

    first commit

---
+++ src/components/my-card/my-card.tsx
@@ -0,0 +1,15 @@
+import { Component,h } from "@stencil/core";
+
+@Component({
+    tag:'my-card',
+    styleUrl:'my-card.css',
+    shadow:true
+})
+
+export class MyCard{
+    render(){
+        return(
+            <div class="my-card-wrapper" >This is a card component</div>
+        );
+    }
+}
